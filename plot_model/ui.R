4fa4fda3a36430429e3fc4d1443dfca871e9b90c
andy <andy.kleinhesselink@gmail.com>
2020-07-03 08:56:38 -0700

    Add shiny app

---
+++ plot_model/ui.R
@@ -0,0 +1,40 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Effects of Region"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       selectInput("ref_family",
+                   "Reference Family:", 
+                   sort(c("Elapidae", 
+                     "Viperidae", 
+                     "Colubridae", 
+                     "Lamprophiidae", 
+                     "Boidae", 
+                     "Cylindrophiidae", 
+                     "Typhlopidae", 
+                     "Pythonidae", 
+                     "Leptotyphlopidae")), 
+                   selected = 'Colubridae')
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("regionPlot"))
+  )
+))
