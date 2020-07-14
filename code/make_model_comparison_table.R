81dd7d9930a173941c36deb1c205823d7c7ff189
andy <andy.kleinhesselink@gmail.com>
2020-07-13 18:15:34 -0700

    Make model tables

--- code/make_model_comparison_table.R
+++ code/make_model_comparison_table.R
@@ -7,12 +7,23 @@ load('output/oos_scored.rda')
 model_loo_table <- read_csv('output/model_comparison_table.csv')
 
 
-oos_accuracy_df  %>% 
+comparison_table <- 
+  oos_accuracy_df  %>% 
   mutate( re = paste0(re, '_RMSE')) %>% 
   spread( re , out_of_sample_RMSE) %>% 
   mutate( model = paste0('m', model)) %>% 
   left_join( model_loo_table %>% 
                rename( model = model_name ), by = 'model') %>% 
-  arrange( looic) %>% 
+  arrange( looic) 
+  
+comparison_table %>%
+  mutate( formula = str_remove( formula, ' \\+ \\(.*\\)')) %>% 
+  mutate( delta_looic = looic[1] - looic ) %>% 
+  rename( `Candidate Model` = model, `Model Formula` = formula, `Out of Sample RMSE 1`= all_RMSE, `Out of Sample RMSE 2` = none_RMSE) %>%
+  select( `Candidate Model`, delta_looic, looic, se_looic, `Out of Sample RMSE 1`, `Out of Sample RMSE 2`, `Model Formula`)  %>% 
+  as.data.frame() %>%
+  mutate_at( c(2:6), .funs = 'round', 2) %>% 
   write_csv('output/full_model_comparison_table.csv')
+
+
 
---
+++ code/make_table_1.R
@@ -0,0 +1,45 @@
+
+rm(list = ls()) 
+# 
+library(tidyverse)
+library(brms)
+
+load('output/full_model_fit_3.rda')
+m3 <- temp_fit
+rm(temp_fit)
+
+my_table <- m3 %>% summary() 
+
+my_table$fixed %>% 
+  data.frame() %>% 
+  mutate( `Fixed Effect` = row.names(. )) %>%
+  select( `Fixed Effect`, Estimate:Tail_ESS) %>% 
+  pander::pander()
+
+my_table
+
+sdev_id <- 
+  my_table$random$id %>%
+  data.frame() %>% 
+  mutate( `Parameter` = row.names(.)) %>%
+  mutate( `Group-Level Effect` = 'Participant ID') %>%
+  select(`Group-Level Effect`, Parameter, Estimate:Tail_ESS) 
+
+sdev_item <- 
+  my_table$random$item %>%
+  data.frame() %>% 
+  mutate( `Parameter` = row.names(.)) %>%
+  mutate(`Group-Level Effect` = 'Image', `Number of Levels` = 131) %>%
+  select(`Group-Level Effect`, `Number of Levels`, Parameter, Estimate:Tail_ESS) 
+
+sdev_species <- 
+  my_table$random$key %>%
+  data.frame() %>% 
+  mutate( `Parameter` = row.names(.)) %>%
+  mutate( `Group-Level Effect` = 'Species') %>%
+  select(`Group-Level Effect`, Parameter, Estimate:Tail_ESS) 
+
+sdev_id
+
+test <- print( my_table )
+
